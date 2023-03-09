aa5b8d2290820063679f284f8ff51c4ea913fa64
Vidaflor Ferreira Costa <vidaflor.lms@gmail.com>
2023-03-09 18:33:04 -0300

    afsf

--- src/api/v1/nqueens/index.js
+++ src/api/v1/nqueens/index.js
@@ -3,14 +3,19 @@ import './index.css';
 import Header from '../../../componets';
 function Nqueens() {
     return (
+        
         <div className="Nqueens">
             <Header/>
-            <h1>Número de Rainhas</h1>
-            <form>
+            <div className="titulo">
+                <h1>Número de Rainhas</h1>  
+            </div>
+                <div className='links'>
+                <form>
                     <label htmlFor="nqueens-input">Número de Rainhas:</label>
                     <input type="number" id="nqueens-input" name="nqueens-input" min="1" max="20" required/>
                     <button type="submit">Jogar</button>
                 </form>
+                </div>
         </div>
     );
 }
