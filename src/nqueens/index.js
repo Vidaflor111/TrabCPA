77d8218a7d0ebe318d7471189cb643f0bf55777d
Vidaflor Ferreira Costa <vidaflor.lms@gmail.com>
2023-03-09 17:52:16 -0300

    Rotaas

---
+++ src/nqueens/index.js
@@ -0,0 +1,137 @@
+
+function App() {
+    return (
+        <div className="App">
+            <h1>Jogos</h1>
+            <div Name="botao-container">
+                <button id="nqueens">N Queens</button>
+                <button id="sudoku">Sudoku</button>
+                <button id="maze">Maze</button>
+                
+            </div>
+
+            <dialog id="nqueens-modal">
+                <form>
+                    <label htmlFor="nqueens-input">Número de Rainhas:</label>
+                    <input type="number" id="nqueens-input" name="nqueens-input" min="1" max="20" required/>
+                    <button type="submit">Jogar</button>
+                </form>
+            </dialog>
+
+            <dialog id="sudoku-modal">
+                <form>
+                    <table id="sudoku-grid">
+                        <tbody>
+                            <tr>
+                                <td><input type="number" min="0" max="9" value="0"/></td>
+                                <td><input type="number" min="0" max="9" value="0"/></td>
+                                <td><input type="number" min="0" max="9" value="0"/></td>
+                                <td><input type="number" min="0" max="9" value="0"/></td>
+                                <td><input type="number" min="0" max="9" value="0"/></td>
+                                <td><input type="number" min="0" max="9" value="0"/></td>
+                                <td><input type="number" min="0" max="9" value="0"/></td>
+                                <td><input type="number" min="0" max="9" value="0"/></td>
+                                <td><input type="number" min="0" max="9" value="0"/></td>
+                            </tr>
+                            <tr>
+                                <td><input type="number" min="0" max="9" value="0"/></td>
+                                <td><input type="number" min="0" max="9" value="0"/></td>
+                                <td><input type="number" min="0" max="9" value="0"/></td>
+                                <td><input type="number" min="0" max="9" value="0"/></td>
+                                <td><input type="number" min="0" max="9" value="0"/></td>
+                                <td><input type="number" min="0" max="9" value="0"/></td>
+                                <td><input type="number" min="0" max="9" value="0"/></td>
+                                <td><input type="number" min="0" max="9" value="0"/></td>
+                                <td><input type="number" min="0" max="9" value="0"/></td>
+                            </tr>
+                            <tr>
+                                <td><input type="number" min="0" max="9" value="0"/></td>
+                                <td><input type="number" min="0" max="9" value="0"/></td>
+                                <td><input type="number" min="0" max="9" value="0"/></td>
+                                <td><input type="number" min="0" max="9" value="0"/></td>
+                                <td><input type="number" min="0" max="9" value="0"/></td>
+                                <td><input type="number" min="0" max="9" value="0"/></td>
+                                <td><input type="number" min="0" max="9" value="0"/></td>
+                                <td><input type="number" min="0" max="9" value="0"/></td>
+                                <td><input type="number" min="0" max="9" value="0"/></td>
+                            </tr>
+                            <tr>
+                                <td><input type="number" min="0" max="9" value="0"/></td>
+                                <td><input type="number" min="0" max="9" value="0"/></td>
+                                <td><input type="number" min="0" max="9" value="0"/></td>
+                                <td><input type="number" min="0" max="9" value="0"/></td>
+                                <td><input type="number" min="0" max="9" value="0"/></td>
+                                <td><input type="number" min="0" max="9" value="0"/></td>
+                                <td><input type="number" min="0" max="9" value="0"/></td>
+                                <td><input type="number" min="0" max="9" value="0"/></td>
+                                <td><input type="number" min="0" max="9" value="0"/></td>
+                            </tr>
+                            <tr>
+                                <td><input type="number" min="0" max="9" value="0"/></td>
+                                <td><input type="number" min="0" max="9" value="0"/></td>
+                                <td><input type="number" min="0" max="9" value="0"/></td>
+                                <td><input type="number" min="0" max="9" value="0"/></td>
+                                <td><input type="number" min="0" max="9" value="0"/></td>
+                                <td><input type="number" min="0" max="9" value="0"/></td>
+                                <td><input type="number" min="0" max="9" value="0"/></td>
+                                <td><input type="number" min="0" max="9" value="0"/></td>
+                                <td><input type="number" min="0" max="9" value="0"/></td>
+                            </tr>
+                            <tr>
+                                <td><input type="number" min="0" max="9" value="0"/></td>
+                                <td><input type="number" min="0" max="9" value="0"/></td>
+                                <td><input type="number" min="0" max="9" value="0"/></td>
+                                <td><input type="number" min="0" max="9" value="0"/></td>
+                                <td><input type="number" min="0" max="9" value="0"/></td>
+                                <td><input type="number" min="0" max="9" value="0"/></td>
+                                <td><input type="number" min="0" max="9" value="0"/></td>
+                                <td><input type="number" min="0" max="9" value="0"/></td>
+                                <td><input type="number" min="0" max="9" value="0"/></td>
+                            </tr>
+                            <tr>
+                                <td><input type="number" min="0" max="9" value="0"/></td>
+                                <td><input type="number" min="0" max="9" value="0"/></td>
+                                <td><input type="number" min="0" max="9" value="0"/></td>
+                                <td><input type="number" min="0" max="9" value="0"/></td>
+                                <td><input type="number" min="0" max="9" value="0"/></td>
+                                <td><input type="number" min="0" max="9" value="0"/></td>
+                                <td><input type="number" min="0" max="9" value="0"/></td>
+                                <td><input type="number" min="0" max="9" value="0"/></td>
+                                <td><input type="number" min="0" max="9" value="0"/></td>
+                            </tr>
+                            <tr>
+                                <td><input type="number" min="0" max="9" value="0"/></td>
+                                <td><input type="number" min="0" max="9" value="0"/></td>
+                                <td><input type="number" min="0" max="9" value="0"/></td>
+                                <td><input type="number" min="0" max="9" value="0"/></td>
+                                <td><input type="number" min="0" max="9" value="0"/></td>
+                                <td><input type="number" min="0" max="9" value="0"/></td>
+                                <td><input type="number" min="0" max="9" value="0"/></td>
+                                <td><input type="number" min="0" max="9" value="0"/></td>
+                                <td><input type="number" min="0" max="9" value="0"/></td>
+                            </tr>
+                            <tr>
+                                <td><input type="number" min="0" max="9" value="0"/></td>
+                                <td><input type="number" min="0" max="9" value="0"/></td>
+                                <td><input type="number" min="0" max="9" value="0"/></td>
+                                <td><input type="number" min="0" max="9" value="0"/></td>
+                                <td><input type="number" min="0" max="9" value="0"/></td>
+                                <td><input type="number" min="0" max="9" value="0"/></td>
+                                <td><input type="number" min="0" max="9" value="0"/></td>
+                                <td><input type="number" min="0" max="9" value="0"/></td>
+                                <td><input type="number" min="0" max="9" value="0"/></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    <button type="submit">Jogar</button>
+                </form>
+            </dialog>
+
+            <dialog id="maze-modal">
+                <button id="maze-solve">Resolver</button>
+            </dialog>
+        </div>
+    );
+}
+
+export default App;
